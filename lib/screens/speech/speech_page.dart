07948f595f05b1abbe50ab85b5e78ec65e6365b9
Carlos Ferreira <conta@especializati.com.br>
2020-10-15 16:06:52 -0300

    Check Auth Start APP

--- lib/screens/speech/speech_page.dart
+++ lib/screens/speech/speech_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+
+import '../../stores/auth.store.dart';
 
 class SpeechScreen extends StatefulWidget {
   SpeechScreen({Key key}) : super(key: key);
@@ -9,19 +13,28 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
+  AuthStore _authStore;
+  FlutterSecureStorage storage = new FlutterSecureStorage();
+
   @override
   void initState() {
     super.initState();
 
     SystemChrome.setEnabledSystemUIOverlays([]);
 
-    _checkAuth().then((value) {
+    _checkAuth().then((bool isAuthenticated) {
+      if (isAuthenticated) {
+        Navigator.pushReplacementNamed(context, '/restaurants');
+        return;
+      }
+
       Navigator.pushReplacementNamed(context, '/login');
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    _authStore = Provider.of<AuthStore>(context);
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Container(
@@ -51,9 +64,15 @@ class _SpeechScreenState extends State<SpeechScreen> {
     );
   }
 
-  Future<String> _checkAuth() async {
-    await Future.delayed(Duration(seconds: 10));
+  Future<bool> _checkAuth() async {
+    final String token = await storage.read(key: 'token_sanctum');
 
-    return '';
+    if (token != null) {
+      final bool isAuthenticated = await _authStore.getMe();
+
+      return isAuthenticated;
+    }
+
+    return false;
   }
 }
